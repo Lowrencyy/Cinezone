c2449d95eca49e3e05ce61d989d89e57060d177c
Lowrencyy <Lowrencyy@gmail.com>
2023-10-22 06:38:28 +0800

    07showdetails

--- js/script.js
+++ js/script.js
@@ -100,8 +100,6 @@ overlayDiv.style.backgroundSize='cover'
   
 }
 
-
-
 //display popular movie 
 async function displayPopularMovies() {
     const {results} = await fetchAPIData ('movie/popular')
@@ -247,7 +245,75 @@ async function displayPopularShows() {
     })
 }
 
+async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1];
+    
 
+    const show = await fetchAPIData(`tv/${showId}`)
+
+    //overlay for background image 
+
+    displayBackgroundImage('tv' , show.backdrop_path);
+
+    const div = document.createElement('div')
+
+    div.innerHTML = `<div class="details-top">
+    <div>
+    ${show.poster_path ?
+        `<img
+        src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+      />`
+       :
+       
+        `<img
+          src="./images/no-image.jpg"
+          class="card-img-top"
+          alt="${show.name}"
+        />`
+      }
+    </div>
+    <div>
+        <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Last Air Date: ${show.last_air_date} </p>
+      <p>
+       ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+      ${show.genres.map((genre) => `<li> ${genre.name}</li>`).join('')} 
+      </ul>
+      <a
+        href="${show.homepage}"
+        target="_blank"
+        class="btn"
+        >Visit TV Show Homepage</a
+      >
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+      <li><span class="text-secondary">Last Episodes to AIr:</span>${show.last_episode_to_air.name}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status} minutes</li>
+      
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group"> ${show.production_companies.map((company) => `<span> ${company.name}</span>`).join(', ')} </div>
+  </div>`;
+
+document.querySelector('#show-details').appendChild(div);
+}
+
+
+
+// display switch
 
 function init() {
     switch(global.currentPage) {
@@ -265,7 +331,7 @@ function init() {
             break;
            
         case '/tv-details.html':
-            console.log('tv-details');
+           displayShowDetails();
             break;
 
         case '/search.html':
